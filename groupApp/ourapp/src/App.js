8fbb2b03390e23fff2002dcb5ef46de3f4be80b6
shyiming007 <115174505+shyiming007@users.noreply.github.com>
2023-06-24 11:32:37 +0800

    initialize four filters

--- groupApp/ourapp/src/App.js
+++ groupApp/ourapp/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg'
 import './App.css'
-import React from 'react'
+import React, { useState } from 'react'
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
 const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
@@ -19,22 +19,81 @@ const MapWithCenter = withGoogleMap(() => (
 ))
 
 function App () {
+  const [timeRange, setTimeRange] = useState([2015, 2019])
+  const [activity, setActivity] = useState('pickup')
+  const [timeSlot, setTimeSlot] = useState('')
+  const [area, setArea] = useState('')
+
+  const handleTimeRangeChange = (event) => {
+    setTimeRange(event.target.value)
+  }
+
+  const handleActivityChange = (event) => {
+    setActivity(event.target.value)
+  }
+
+  const handleTimeSlotChange = (event) => {
+    setTimeSlot(event.target.value)
+  }
+
+  const handleAreaChange = (event) => {
+    setArea(event.target.value)
+  }
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
-      {/* content */}
-      <div
-        style={{
-          position: 'absolute',
-          bottom: '0',
-          left: '0',
-          right: '30%',
-          top: '30%',
-        }}
-      >
-        <MapWithCenter
-          containerElement={<div style={{ height: '100%' }} />}
-          mapElement={<div style={{ height: '100%' }} />}
+    <div>
+      <div>
+
+        <h2>Time Filter</h2>
+        <input
+          type="range"
+          min={2015}
+          max={2019}
+          value={timeRange}
+          onChange={handleTimeRangeChange}
         />
+
+        <h2>Vehicle Activity Choice</h2>
+        <select value={activity} onChange={handleActivityChange}>
+          <option value="pickup">Pick-up</option>
+          <option value="dropoff">Drop-off</option>
+        </select>
+
+        <h2>Time Slot Choice</h2>
+        <input
+          type="time"
+          value={timeSlot}
+          onChange={handleTimeSlotChange}
+        />
+
+        <h2>Area Choice</h2>
+        <select value={area} onChange={handleAreaChange}>
+          <option value="">All Areas</option>
+          <option value="area1">Area 1</option>
+          <option value="area2">Area 2</option>
+          <option value="area3">Area 3</option>
+        </select>
+
+        {/* 其他组件内容 */}
+      </div>
+
+
+
+      <div style={{ position: 'relative', width: '100%', height: '100vh' }}>
+        {/* content */}
+        <div
+          style={{
+            position: 'absolute',
+            bottom: '0',
+            left: '0',
+            right: '30%',
+            top: '5%',
+          }}
+        >
+          <MapWithCenter
+            containerElement={<div style={{ height: '100%' }} />}
+            mapElement={<div style={{ height: '100%' }} />}
+          />
+        </div>
       </div>
     </div>
   )
