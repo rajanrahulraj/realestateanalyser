257ba077625a2dcd7a5dbab7172b861eab37ce0d
shyiming007 <115174505+shyiming007@users.noreply.github.com>
2023-07-10 15:55:32 +0800

    Create Mymap.jsx

---
+++ groupApp/ourapp/src/Mymap.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react'
+import React from 'react'
+
+class Mymap extends Component {
+  state = {}
+  render() {
+    return (
+      <div>
+        <h1>Taxi Zones</h1>
+      </div>
+    )
+  }
+}
+
+export default Mymap
